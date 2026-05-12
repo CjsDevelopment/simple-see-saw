d8ead83f48e422fe07b2353f7f5c02e3e672c1d4
CjsDevelopment <nzspeedcubing@gmail.com>
2026-05-12 17:50:42 -0400

    new stuff added
    index.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getFirestore,collection,addDoc,query,orderBy,limit,getDocs  } from "firebase/app";
+import { getFirestore,collection,addDoc,query,orderBy,limit,getDocs  } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
