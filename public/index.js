d8b1eff1cd24f667fc24e301efa78a8a04d61b97
CjsDevelopment <nzspeedcubing@gmail.com>
2026-05-26 16:40:59 -0400

    Clean up
    removed all the extra stuff

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getFirestore,collection,addDoc,query,orderBy,limit,getDocs  } from "firebase/firestore";
+import { getFirestore,collection,addDoc,query,orderBy,limit,getDocs  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
